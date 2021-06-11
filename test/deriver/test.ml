2aaf544991d6a434bfab0acd8027313c93b8fe60
Valentin Chaboche <valentinchb@gmail.com>
2021-06-11 13:43:49 +0200

    Add tree good tests

--- test/deriver/test.ml
+++ test/deriver/test.ml
@@ -431,6 +431,73 @@ let test_variant () =
   in
   check_eq ~expected ~actual "deriving variant"
 
+let test_tree () =
+  let expected =
+    [
+      [%stri
+        include struct
+          let rec arb_tree () = arb_tree' 5
+
+          and arb_tree' = function
+            | 0 -> QCheck.oneof [ QCheck.always Leaf ]
+            | n ->
+                QCheck.oneof
+                  [
+                    QCheck.always Leaf;
+                    QCheck.map
+                      (fun (arb_0, (arb_1, arb_2)) ->
+                        Node (arb_0, arb_1, arb_2))
+                      (QCheck.pair
+                         QCheck.int
+                         (QCheck.pair (arb_tree' (n - 1)) (arb_tree' (n - 1))));
+                  ]
+
+          let arb_tree = arb_tree ()
+        end];
+      [%stri
+        include struct
+          let rec arb_expr () = arb_expr' 5
+
+          and arb_expr' = function
+            | 0 ->
+                QCheck.oneof
+                  [ QCheck.map (fun arb_0 -> Value arb_0) QCheck.int ]
+            | n ->
+                QCheck.oneof
+                  [
+                    QCheck.map (fun arb_0 -> Value arb_0) QCheck.int;
+                    QCheck.map
+                      (fun (arb_0, (arb_1, arb_2)) -> If (arb_0, arb_1, arb_2))
+                      (QCheck.pair
+                         (arb_expr' (n - 1))
+                         (QCheck.pair (arb_expr' (n - 1)) (arb_expr' (n - 1))));
+                    QCheck.map
+                      (fun (arb_0, arb_1) -> Eq (arb_0, arb_1))
+                      (QCheck.pair (arb_expr' (n - 1)) (arb_expr' (n - 1)));
+                    QCheck.map
+                      (fun (arb_0, arb_1) -> Lt (arb_0, arb_1))
+                      (QCheck.pair (arb_expr' (n - 1)) (arb_expr' (n - 1)));
+                  ]
+
+          let arb_expr = arb_expr ()
+        end];
+    ]
+  in
+  let actual =
+    f'
+    @@ extract'
+         [
+           [%stri type tree = Leaf | Node of int * tree * tree];
+           [%stri
+             type expr =
+               | Value of int
+               | If of expr * expr * expr
+               | Eq of expr * expr
+               | Lt of expr * expr];
+         ]
+  in
+  check_eq ~expected ~actual "deriving variant"
+
 let () =
   Alcotest.(
     run
@@ -458,5 +525,6 @@ let () =
             test_case "deriving constructors" `Quick test_konstr;
             test_case "deriving record" `Quick test_record;
             test_case "deriving variant" `Quick test_variant;
+            test_case "deriving tree like" `Quick test_tree;
           ] );
       ])
